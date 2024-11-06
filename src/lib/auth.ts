78ee61e61b199846d4accbc0daf8bc50cc09ce49
isirmt - Ito Miri <52734487+isirmt@users.noreply.github.com>
2024-11-06 13:21:53 +0900

    update: extending Session interface

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,25 +1,35 @@
-import NextAuth from 'next-auth';
+import NextAuth, { Session } from 'next-auth';
+import { AdapterUser } from 'next-auth/adapters';
+import { JWT } from 'next-auth/jwt';
 import GitHubProvider from 'next-auth/providers/github';
+
+declare module 'next-auth' {
+  interface Session {
+    access_token?: string;
+    user: AdapterUser;
+  }
+}
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     GitHubProvider({}),
   ],
   callbacks: {
-    // eslint-disable-next-line no-unused-vars
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async jwt({ token, user, account, profile }) {
       if (profile && account) {
-        console.log("account", account)
         token.username = profile.login;
         token.access_token = account.access_token;
       }
       return token;
     },
-    async session({ session, token }) {
-      // console.log("token", token)
-      // console.log("session", session)
+    async session({ session, token }: {
+      session: Session,
+      token: JWT
+    }) {
+      // token から session へ access_token を追加
       session.user.id = token.username as string;
-      session.access_token = token.access_token as string
+      session.access_token = token.access_token as string;
       return session;
     },
   },
