b57ff532b28856b72e1e3df3831400e1db84d410
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-11-10 02:53:45 +0900

    add: debug home text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-
+    <main className='p-4'>
+      <Link href="/tree">Go to /tree</Link>
     </main>
   );
 }
