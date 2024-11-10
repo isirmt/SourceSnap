494072206437d0b0db4b00da64f7d38f66c2b215
isirmt <r119306@g.kobe-kosen.ac.jp>
2024-11-10 20:25:01 +0900

    add: session button on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 import { auth } from "@/lib/auth";
 import { Session } from "next-auth";
+import SessionButton from "./SessionButton";
+import React from "react";
 
 export async function Header() {
   const session: Session | null = await auth();
   return <header className="w-full h-12 bg-slate-100 justify-center flex border-b border-slate-300">
-    <div className="max-w-[72rem] px-4 items-center w-full h-full flex justify-between">
-      <div className="text-xl font-bold text-slate-800">
+    <div className="max-w-[72rem] px-4 items-stretch w-full h-full flex justify-between">
+      <div className="text-xl font-bold text-slate-800 flex items-center">
         Tree Downloader
       </div>
-      {session ? (
+
+      <div className="group h-auto flex items-center">
         <div className="flex gap-1 items-center text-slate-800">
-          <img alt="user-icon" src={session.user.image!} className="size-8 rounded-full" />
-          {session.user.id}
-        </div>) : <div></div>}
+          {session ? (
+            <React.Fragment>
+              <img alt="user-icon" src={session.user.image!} className="size-8 rounded-full" />
+              {session.user.id}
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <div className="size-8 rounded-full bg-slate-200" />
+              Not signed in
+            </React.Fragment>)}
+        </div>
+        <div className="hidden group-hover:block absolute top-10 bg-slate-50 border right-0 px-3 rounded">
+          <SessionButton />
+        </div>
+      </div>
     </div>
   </header>
 }
--- src/components/SessionButton.tsx
+++ src/components/SessionButton.tsx
@@ -11,8 +11,8 @@ export default async function SessionButton() {
             await signIn('GitHub');
           }}
         >
-          <button className='my-5 rounded border border-blue-400 bg-slate-50 px-5 py-2 font-bold text-blue-400 transition-colors hover:bg-blue-100'>
-            GitHubでサインイン
+          <button className='my-5 rounded border border-blue-400 bg-transparent px-5 py-2 font-bold text-blue-400 transition-colors hover:bg-blue-100'>
+            <span className='i-tabler-brand-github translate-y-1 mr-1' />SignIn with GitHub
           </button>
         </form>
       )}
@@ -23,8 +23,8 @@ export default async function SessionButton() {
             await signOut();
           }}
         >
-          <button className='my-5 rounded border border-red-400 bg-slate-50 px-5 py-2 font-bold text-red-400 transition-colors hover:bg-red-100'>
-            サインアウト
+          <button className='my-5 rounded border border-red-400 bg-transparent px-5 py-2 font-bold text-red-400 transition-colors hover:bg-red-100'>
+            SignOut
           </button>
         </form>
       )}
