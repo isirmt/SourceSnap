184b1541ea7e81e85d425210e3639832b62e8658
isirmt - Ito Miri <52734487+isirmt@users.noreply.github.com>
2024-11-11 10:23:20 +0900

    add: pagination

--- src/components/UserRepoList.tsx
+++ src/components/UserRepoList.tsx
@@ -12,40 +12,42 @@ export default function UserRepoList({ octokit, onSelectRepo }: RepoListProps) {
   type GitHubUserRepos = GetResponseTypeFromEndpointMethod<NonNullable<typeof octokit>['repos']['listForAuthenticatedUser']>;
   type GitHubUserStarredRepos = GetResponseTypeFromEndpointMethod<NonNullable<typeof octokit>['activity']['listReposStarredByAuthenticatedUser']>;
 
-  const [userRepos, setUserRepos] = useState<GitHubUserRepos>();
-  const [starredRepos, setStarredRepos] = useState<GitHubUserStarredRepos>();
+  const [userRepos, setUserRepos] = useState<GitHubUserRepos['data']>([]);
+  const [starredRepos, setStarredRepos] = useState<GitHubUserStarredRepos['data']>([]);
+  const [userPage, setUserPage] = useState(1);
+  const [starredPage, setStarredPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (octokit) {
-      const fetchRepos = async () => {
+    const fetchRepos = async (_userPage = userPage, _starredPage = starredPage) => {
+      if (octokit) {
         try {
-          const userReposResponse = await octokit.repos.listForAuthenticatedUser();
-          const starredReposResponse = await octokit.activity.listReposStarredByAuthenticatedUser();
-          setUserRepos(userReposResponse);
-          setStarredRepos(starredReposResponse);
+          setLoading(true);
+          const userReposResponse = await octokit.repos.listForAuthenticatedUser({ per_page: 30, page: _userPage });
+          const starredReposResponse = await octokit.activity.listReposStarredByAuthenticatedUser({ per_page: 30, page: _starredPage });
+
+          setUserRepos((prevRepos) => [...prevRepos, ...userReposResponse.data]);
+          setStarredRepos((prevRepos) => [...prevRepos, ...starredReposResponse.data]);
         } catch (err) {
           console.error('Failed to get repos:', err);
+        } finally {
+          setLoading(false);
         }
-      };
-      fetchRepos();
-    }
-  }, [octokit]);
+      }
+    };
+
+    fetchRepos(userPage, starredPage);
+  }, [octokit, userPage, starredPage]);
+
+  const loadMoreUserRepos = () => setUserPage((prevPage) => prevPage + 1);
+  const loadMoreStarredRepos = () => setStarredPage((prevPage) => prevPage + 1);
 
   return (
     <div className='max-w-full w-[40rem]'>
-      <h2 className='font-bold'>Candidate Repository List</h2>
+      <h2 className='font-bold'>Your Repositories</h2>
       <ul className='block w-full border-x border-slate-200 rounded-lg overflow-clip'>
-        {userRepos?.data.map((repo) => (
-          <li key={repo.id}>
-            <RepoContext
-              owner={repo.owner.login}
-              repo={repo.name}
-              onSelectRepo={() => onSelectRepo(repo.owner.login, repo.name)}
-            />
-          </li>
-        ))}
-        {starredRepos?.data.map((repo) => (
-          <li key={repo.id}>
+        {userRepos.map((repo) => (
+          <li key={`user:${repo.id}`}>
             <RepoContext
               owner={repo.owner.login}
               repo={repo.name}
@@ -54,6 +56,24 @@ export default function UserRepoList({ octokit, onSelectRepo }: RepoListProps) {
           </li>
         ))}
       </ul>
+      <button onClick={loadMoreUserRepos} disabled={loading} className="p-2 bg-blue-500 text-white disabled:bg-gray-400 disabled:text-gray-700 rounded my-2 hover:bg-blue-600 transition-colors">
+        {loading ? 'Loading...' : 'Load More'}
+      </button>
+      <h2 className='font-bold'>Your Starred Repositories</h2>
+      <ul className='block w-full border-x border-slate-200 rounded-lg overflow-clip'>
+        {starredRepos.map((repo) => (
+          <li key={`star:${repo.id}`}>
+            <RepoContext
+              owner={repo.owner.login}
+              repo={repo.name}
+              onSelectRepo={() => onSelectRepo(repo.owner.login, repo.name)}
+            />
+          </li>
+        ))}
+      </ul>
+      <button onClick={loadMoreStarredRepos} disabled={loading} className="p-2 bg-blue-500 text-white disabled:bg-gray-400 disabled:text-gray-700 rounded my-2 hover:bg-blue-600 transition-colors">
+        {loading ? 'Loading...' : 'Load More'}
+      </button>
     </div>
   );
 }
