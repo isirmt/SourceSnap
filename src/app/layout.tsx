98ebdd8925231802c1cf65567ea5625ba8536426
isirmt - Ito Miri <52734487+isirmt@users.noreply.github.com>
2024-11-13 17:19:56 +0900

    add: nextjs-toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import React from 'react';
+import NextTopLoader from 'nextjs-toploader';
 import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header';
 import { generateMetadataTemplate } from '@/lib/SEO';
@@ -17,6 +18,14 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className='flex flex-col'>
+        <NextTopLoader
+          color='#3B82F6'
+          template='<div style="height: .15rem;" class="bar" role="bar"><div class="peg"></div></div> 
+                    <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          shadow={false}
+          showSpinner={false}
+          zIndex={100}
+        />
         <Header />
         <div className='flex h-auto min-h-[calc(100svh_-_3rem)] flex-col'>
           <div className='relative flex-grow'>{children}</div>
